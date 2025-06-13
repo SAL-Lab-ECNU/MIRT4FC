90725bd7940c463c8b54f0846f2e241dc34df0f3
xpy-ECNU <113600692+xpy-ECNU@users.noreply.github.com>
2025-06-13 20:46:59 +0800

    Delete R directory

--- R/data.sim_ZG.R
+++
@@ -1,8 +0,0 @@
-data.sim_ZG <- function(item.par=item.par,theta=theta,BID=BID,blocksize=3,res='rank'){
-  if(blocksize==2){
-    Y <- data.sim_ZG_2(item.par=item.par,theta=theta,BID=BID)
-  }else{
-    cat("The model is not included")
-  }
-  Y
-}
