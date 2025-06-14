dababfc7cd3ed42744506ab1fec12ad6953df07a
xpy-ECNU <113600692+xpy-ECNU@users.noreply.github.com>
2025-06-14 23:20:42 +0800

    Delete R directory

--- R/itemSE_bootstrap.R
+++
@@ -1,6 +0,0 @@
-itemSE_bootstrap<- function(plist,blocksize,IN,model,J,D){
-  if(model=='2PL'){
-    x=itemSE_bootstrap_2PL(plist=plist,blocksize=blocksize,IN=IN,J=J,D=D)
-  }
-  x
-}
--- R/utils.R
+++
@@ -1,5 +0,0 @@
-batch.var <- function(plist,n){
-  phi.bar <- sapply(plist,rowMeans) # of par x n
-  phi.hat <- rowMeans(Reduce(cbind,plist))
-  rowMeans((phi.bar-phi.hat)^2)/(n-1)
-}
